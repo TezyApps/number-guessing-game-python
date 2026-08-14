355d3bcac50822ee70a3e6d327ebf15efd40bd54
Tezy Apps <tezyapps@gmail.com>
2026-08-14 12:54:13 +1000

    Level 1: 1. Define a secret number 2. Define a method to start the game   a. ask user to input a guess   b. compare the user input number matches with secret number   c. print respective won/lost message

--- game/src/game/__init__.py
+++ game/src/game/__init__.py
@@ -1,3 +1,18 @@
+
+def secret_number() -> int:
+    return 56
+
+def start_game() -> None:
+    user_input = input("Enter your guess : ")
+    if int(user_input) == secret_number():
+        print(f"{user_input} 🏆")
+        print("Congrats, you've guessed the number correct!")
+    else:
+        print(f"{user_input} ❌")
+        print(f"Not Quite! Please try again")
+
 def main() -> None:
     user_name = input("What should we call you? ")
     print(f"Hello {user_name} 👋, Welcome to the 'Number Guessing Game'!")
+
+    start_game()
